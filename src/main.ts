02e87f6d5832a6d2c1ee8e9e4ed23c640fb1729c
JanyanP <janyan.rodriguez.271@gmail.com>
2021-11-10 01:47:43 -0600

    Firebase
    Inicialización de firebase y realtime database

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/quotes */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,3 +11,35 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+
+
+
+//INICIALIZACION FIREBASE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDF-LcDPy8fDKCSoBhCJRMDAvjcjWH80VY",
+  authDomain: "suzannecollins-aa6a4.firebaseapp.com",
+  projectId: "suzannecollins-aa6a4",
+  storageBucket: "suzannecollins-aa6a4.appspot.com",
+  messagingSenderId: "573507894452",
+  appId: "1:573507894452:web:be23bc84b8f92975806be1",
+  measurementId: "G-WSC0QPKXXB",
+  databaseURL: "https://suzannecollins-aa6a4-default-rtdb.firebaseio.com/",
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Get a reference to the database service
+const database = getDatabase(app);
